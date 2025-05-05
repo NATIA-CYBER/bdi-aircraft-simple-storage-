7f38d537ca99f5255f742b74aa9615afa56485fb
Natia Utrugashvili <natiautrugashvili@gmail.com>
2025-05-05 05:15:15 +0400

    Remove deployment files

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install dependencies
-pip install -r requirements.txt
-
-# Run the FastAPI application with sudo to bind to port 80
-sudo uvicorn src.main:app --host 0.0.0.0 --port 80
